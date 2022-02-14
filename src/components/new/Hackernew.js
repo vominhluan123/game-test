0a78cca4e0f83cb4bb0a69dbc0f8a550b5c6bf96
vominhluan123 <minhluan2555@gmail.com>
2022-02-14 01:35:03 -0800

    update

--- src/components/new/Hackernew.js
+++ src/components/new/Hackernew.js
@@ -1,19 +1,20 @@
 import React, { useRef, useState } from "react";
 import axios from "axios";
-import lodash from "lodash";
+// import lodash from "lodash";
 
 const Hackernew = () => {
   const [hits, setHits] = useState([]);
-  const [query, setQuery] = useState("react");
+  const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
   const handleFetchData = useRef({});
+  const [url, setUrl] = useState(
+    `https://hn.algolia.com/api/v1/search?query=${query}`
+  );
   handleFetchData.current = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
-      );
+      const response = await axios.get(url);
       setHits(response.data?.hits);
       setLoading(false);
     } catch (error) {
@@ -21,27 +22,52 @@ const Hackernew = () => {
       setErrorMessage(`The error happenend ${error}`);
     }
   };
-  const handleUpdateQuery = lodash.debounce((e) => {
-    setQuery(e.target.value);
-  }, 500);
+  // const handleUpdateQuery = lodash.debounce((e) => {
+  //   setQuery(e.target.value);
+  // }, 500);
   React.useEffect(() => {
     handleFetchData.current();
-  }, [query]);
+  }, [url]);
   return (
-    <div>
-      <input
-        type="text"
-        className="border border-green-500 text-black p-5 mb-5"
-        defaultValue={query}
-        onChange={handleUpdateQuery}
-      />
+    <div className="bg-white mx-auto mt-5 mb-5 p-5 rounded-lg shadow-md w-2/4">
+      <div className="flex mb-5 gap-x-5">
+        <input
+          type="text"
+          className="border border-gray-200 p-5 block w-full rounded-md  transition-all focus:border-blue-400 outline-none"
+          placeholder="Keyword..."
+          defaultValue={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+          }
+          className="bg-blue-500 text-white font-semibold p-5 rounded-md"
+          style={{
+            opacity: loading ? "0.25" : "1",
+          }}
+        >
+          Fetching
+        </button>
+      </div>
       {loading && (
-        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin"></div>
+        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin mx-auto my-10"></div>
       )}
-      {!loading && errorMessage && <p>{errorMessage}</p>}
-      {!loading &&
-        hits.length > 0 &&
-        hits.map((item, index) => <h3 key={item.title}>{item.title}</h3>)}
+      {!loading && errorMessage && (
+        <p className="text-red-400 my-5">{errorMessage}</p>
+      )}
+      <div className="flex flex-wrap gap-5">
+        {!loading &&
+          hits.length > 0 &&
+          hits.map((item, index) => {
+            if (!item.title || item.title.length <= 0) return null;
+            return (
+              <h3 key={item.title} className="p-3 bg-gray-100">
+                {item.title}
+              </h3>
+            );
+          })}
+      </div>
     </div>
   );
 };
